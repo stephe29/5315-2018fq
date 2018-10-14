851c7816ae3537b33e823bfea161b8443189a65e
cameron <stephe29@seattleu.edu>
2018-10-14 10:38:21 -0700

    hw2

--- empdata.js
+++ empdata.js
@@ -1,124 +1,184 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
-      "extension": "8422"
+      "product_id": "10001",
+      "product_category": "Beer",
+      "product_category_id": "100",
+      "product_name": "Blue Moon",
+      "list_price": "$14.99",
+      "quantity_on_hand": "53",
+      "last_months_sales": "22",
+      "customer_raiting": "3.8"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "product_id": "10002",
+      "product_category": "Beer",
+      "product_category_id": "100",
+      "product_name": "Stella Artois",
+      "list_price": "$13.99",
+      "quantity_on_hand": "102",
+      "last_months_sales": "57",
+      "customer_raiting": "4.7"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+      "product_id": "10003",
+      "product_category": "Beer",
+      "product_category_id": "100",
+      "product_name": "Spaten Oktoberfest",
+      "list_price": "$8.99",
+      "quantity_on_hand": "77",
+      "last_months_sales": "153",
+      "customer_raiting": "4.8"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
-      "extension": "5407"
+      "product_id": "10004",
+      "product_category": "Beer",
+      "product_category_id": "100",
+      "product_name": "Bud Light",
+      "list_price": "$10.49",
+      "quantity_on_hand": "222",
+      "last_months_sales": "117",
+      "customer_raiting": "3.6"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
+      "product_id": "10005",
+      "product_category": "Beer",
+      "product_category_id": "100",
+      "product_name": "Coors Light",
+      "list_price": "$10.49",
+      "quantity_on_hand": "79",
+      "last_months_sales": "132",
+      "customer_raiting": "4.3"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
+      "product_id": "10006",
+      "product_category": "Beer",
+      "product_category_id": "100",
+      "product_name": "Elysian Space Dust IPA",
+      "list_price": "$18.99",
+      "quantity_on_hand": "34",
+      "last_months_sales": "52",
+      "customer_raiting": "4.5"
     },
     {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
+      "product_id": "20001",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Lagavulin 16 Yr",
+      "list_price": "$69.99",
+      "quantity_on_hand": "55",
+      "last_months_sales": "62",
+      "customer_raiting": "4.7"
     },
     {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
+      "product_id": "20002",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Laphroaig 10 Yr",
+      "list_price": "$44.99",
+      "quantity_on_hand": "65",
+      "last_months_sales": "79",
+      "customer_raiting": "4.8"
     },
     {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
+      "product_id": "20003",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Ardbeg 10 Yr",
+      "list_price": "$62.99",
+      "quantity_on_hand": "34",
+      "last_months_sales": "42",
+      "customer_raiting": "4.6"
     },
     {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
+      "product_id": "20004",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Kilchoman Loch Grom",
+      "list_price": "$99.99",
+      "quantity_on_hand": "26",
+      "last_months_sales": "17",
+      "customer_raiting": "4.9"
     },
     {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
+      "product_id": "20005",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Bruichladdich Octomore 6.1",
+      "list_price": "$159.99",
+      "quantity_on_hand": "12",
+      "last_months_sales": "32",
+      "customer_raiting": "4.9"
     },
     {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
+      "product_id": "20006",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Laphroaig Quarter Cask",
+      "list_price": "$69.99",
+      "quantity_on_hand": "49",
+      "last_months_sales": "73",
+      "customer_raiting": "4.8"
     },
     {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
+      "product_id": "20007",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Ardbeg Corryvreckan",
+      "list_price": "$109.99",
+      "quantity_on_hand": "38",
+      "last_months_sales": "77",
+      "customer_raiting": "4.9"
     },
     {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
+      "product_id": "20008",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Bruichladdich Port Charlotte",
+      "list_price": "$69.99",
+      "quantity_on_hand": "24",
+      "last_months_sales": "33",
+      "customer_raiting": "5.0"
     },
     {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
+      "product_id": "20009",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Oban 14 Yr",
+      "list_price": "$63.99",
+      "quantity_on_hand": "61",
+      "last_months_sales": "49",
+      "customer_raiting": "4.8"
+    },
+    {
+      "product_id": "20010",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Talisker 10 Yr",
+      "list_price": "$71.99",
+      "quantity_on_hand": "32",
+      "last_months_sales": "17",
+      "customer_raiting": "4.6"
+    },
+    {
+      "product_id": "20011",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Jura 10 Yr",
+      "list_price": "$59.99",
+      "quantity_on_hand": "23",
+      "last_months_sales": "29",
+      "customer_raiting": "4.0"
+    },
+    {
+      "product_id": "20012",
+      "product_category": "Scotch",
+      "product_category_id": "200",
+      "product_name": "Caol Ila 12 Yr",
+      "list_price": "$69.99",
+      "quantity_on_hand": "25",
+      "last_months_sales": "14",
+      "customer_raiting": "4.1"
     }
   ]
 }
